22541ac464fd6477d8966dfaa15f4a669741d968
Nelson Ramirez <xhyskoax@gmail.com>
2023-05-22 21:06:34 -0400

    agregando morgan al proyecto y modificando rutas

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
+var morgan = require("morgan");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 let usuarios = [
     {
@@ -15,9 +17,7 @@ let usuarios = [
     },
 ];
 
-app.listen(3000, () =>
-    console.log("Servidor escuchando en http://localhost:3000")
-);
+app.listen(3000, () => console.log("Servidor escuchando..."));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
