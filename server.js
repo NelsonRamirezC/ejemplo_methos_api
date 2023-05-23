2809cd1fbd458c1ad70960c4772331f872397a67
Nelson Ramirez <xhyskoax@gmail.com>
2023-05-22 22:54:43 -0400

    se agrega persistencia a usuarios

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
-var morgan = require("morgan");
+const fs = require("fs");
+const morgan = require("morgan");
 const app = express();
 
 app.use(cors());
@@ -9,13 +10,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 
-let usuarios = [
-    {
-        id: "f9de07",
-        nombre: "Carlos",
-        email: "carlos@gmail.com",
-    },
-];
+const leerUsuarios = () => {
+    let usuarios = JSON.parse(
+        fs.readFileSync(__dirname + "/usuarios.json", "utf8")
+    );
+    return usuarios.data;
+};
+
+const guardarUsuarios = (usuarios) => {
+    let users = JSON.parse(
+        fs.readFileSync(__dirname + "/usuarios.json", "utf8")
+    );
+    users.data = usuarios;
+    fs.writeFileSync(
+        __dirname + "/usuarios.json",
+        JSON.stringify(users, null, 4),
+        "utf8"
+    );
+};
 
 app.listen(3000, () => console.log("Servidor escuchando..."));
 
@@ -24,11 +36,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/usuarios", (req, res) => {
+    let usuarios = leerUsuarios();
     res.status(200).send({ status: 200, usuarios });
 });
 
 app.get("/usuarios/:id", (req, res) => {
     let { id } = req.params;
+    let usuarios = leerUsuarios();
     let found = usuarios.find((usuario) => usuario.id == id);
     if (found) {
         res.status(200).send({ status: 200, usuario: found });
@@ -44,17 +58,21 @@ app.post("/usuarios", (req, res) => {
         nombre,
         email,
     };
+    let usuarios = leerUsuarios();
     usuarios.push(nuevoUsuario);
+    guardarUsuarios(usuarios);
 
     res.status(201).send({ status: 201, usuario: nuevoUsuario });
 });
 
 app.put("/usuarios", (req, res) => {
     let { id, nombre, email } = req.body;
+    let usuarios = leerUsuarios();
     let found = usuarios.find((usuario) => usuario.id == id);
     if (found) {
         found.nombre = nombre;
         found.email = email;
+        guardarUsuarios(usuarios);
         res.status(201).send({
             status: 201,
             usuario: found,
@@ -70,9 +88,11 @@ app.put("/usuarios", (req, res) => {
 
 app.delete("/usuarios/:id", (req, res) => {
     let { id } = req.params;
+    let usuarios = leerUsuarios();
     let found = usuarios.find((usuario) => usuario.id == id);
     if (found) {
         usuarios = usuarios.filter((usuario) => usuario.id != id);
+        guardarUsuarios(usuarios);
         res.status(200).send({ status: 200, message: "Usuario eliminado." });
     } else {
         res.status(404).send({ status: 404, message: "Usuario no encontrado" });
